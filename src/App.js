def4a872829a254a77274fabead81275795b2f95
James Bryan <hikage.jvb3@gmail.com>
2022-10-25 15:39:20 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import "./styles/styles.css";
 import { Routes, Route, useLocation } from "react-router-dom";
-import MobileNavBar from "./MobileNavBar";
-// import FrontPageHeader from "./components/FrontPage/FrontPageHeader";
-// import AboutMe from "./components/FrontPage/AboutMe";
-// import Contact from "./components/FrontPage/Contact";
-
-// import DropDownMenu from "./components/FrontPage/DropDownMenu";
+import { useState, useEffect } from "react";
 import FrontPageGrid from "./components/FrontPage/FrontPageGrid";
 import PortfolioIndexGrid from "./components/PortfolioIndex/PortfolioIndexGrid";
 import ContactPageGrid from "./components/ContactPage/ContactPageGrid";
-import Projects from "./components/PortfolioIndex/Projects";
-import { useState, useEffect } from "react";
+
 import ManageGrid from "./components/Projects/Manage/ManageGrid";
 import BookmarkGrid from "./components/Projects/Bookmark/BookmarkGrid";
 import InsureGrid from "./components/Projects/Insure/InsureGrid";
 import FyloGrid from "./components/Projects/Fylo/FyloGrid";
-import FrontPageHeader from "./components/FrontPage/FrontPageHeader";
-import AboutMe from "./components/FrontPage/AboutMe";
-import Footer from "./components/FrontPage/Footer";
-import BookmarkBrowserBtn from "./BrowserBtn";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
